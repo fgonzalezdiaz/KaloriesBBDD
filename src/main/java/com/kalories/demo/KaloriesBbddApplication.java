b5ca5d72994231d71e8889c55c510ac8aa24460a
fgonzalezdiaz <fgonzalezd24042005@gmail.com>
2026-01-21 19:20:50 +0100

    Creacio del projecte

---
+++ src/main/java/com/kalories/demo/KaloriesBbddApplication.java
@@ -0,0 +1,13 @@
+package com.kalories.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaloriesBbddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaloriesBbddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalories/demo/KaloriesBbddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalories.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaloriesBbddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
