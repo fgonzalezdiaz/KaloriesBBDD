b82ccde5f0c2b6f31b324f3e384bc3bf2127c5d7
fgonzalezdiaz <fgonzalezd24042005@gmail.com>
2026-01-21 19:51:19 +0100

    BBDD conectada y funcional - INICIALIZACION COMPLETADA

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    contrasena VARCHAR(255) NOT NULL,
+    genero VARCHAR(50),
+    fecha_nacimiento DATE,
+    altura INT,
+    peso DECIMAL(5,2),
+    nivel_actividad TINYINT CHECK (nivel_actividad IN (0, 1, 2)),
+    objetivo TINYINT CHECK (objetivo in (0, 1, 2))
+);
