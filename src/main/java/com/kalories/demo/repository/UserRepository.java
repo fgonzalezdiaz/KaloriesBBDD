e90680949d69f6afc2a285936b777a2117c91056
fgonzalezdiaz <fgonzalezd24042005@gmail.com>
2026-01-28 19:55:28 +0100

    Treball a classe

--- src/main/java/com/kalories/demo/repository/UserRepository.java
+++ src/main/java/com/kalories/demo/repository/UserRepository.java
@@ -1,5 +1,58 @@
 package com.kalories.demo.repository;
 
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.kalories.demo.model.User;
+
 public class UserRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    private static final class UserRowMapper implements RowMapper<User>{
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException{
+            return new User(
+                rs.getLong("id"),
+                rs.getString("email"),
+                rs.getString("contrasena"),
+                rs.getString("genero"),
+                rs.getDate("fecha_nacimiento").toLocalDate(),
+                rs.getInt("altura"),
+                rs.getBigDecimal("peso"),
+                rs.getInt("nivel_actividad"),
+                rs.getInt("objetivo"),
+                rs.getString("img_path")
+            );
+        }
+    }
+    
+    // Read todos
+    public List<User> findAll(){
+        String sql = "Select * from users";
+        return jdbcTemplate.query(sql, new UserRowMapper());
+    }
+
+    // Crear un usuario
+    public int save(String email, String contrasena, String genero, LocalDate fechaNacimiento, Integer altura, BigDecimal peso, Integer nivelActividad, Integer objetivo, String imgPath){
+        String sql = "INSERT INTO USERS (EMAIL, CONTRASENA, GENERO, FECHA_NACIMIENTO, ALTURA, PESO, NIVEL_ACTIVIDAD, OBJETIVO, IMG_PATH) VALUES (?,?,?,?,?,?,?,?,?)";
+        return jdbcTemplate.update(sql, email, contrasena, genero, fechaNacimiento, altura, peso, nivelActividad, objetivo, imgPath);
+    }
+
+    // Read por id
+    public List<User> findById(Long id){
+        String sql = "Select * from users where id == ?";
+        return jdbcTemplate.query(sql, new UserRowMapper(), id);
+    }
+
+    
 
 }
--- src/main/java/com/kalories/demo/service/UserService.java
+++ src/main/java/com/kalories/demo/service/UserService.java
@@ -1,5 +1,49 @@
 package com.kalories.demo.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kalories.demo.logging.CustomLogging;
+import com.kalories.demo.model.User;
+import com.kalories.demo.repository.UserRepository;
+
+@Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        CustomLogging.info("Consultant tots els users", "UserService", "findAll");
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        CustomLogging.info("Consultant user per id", "UserService", "findById");
+        List<User> users = userRepository.findById(id);
+        if (users.isEmpty()) {
+            CustomLogging.error("L'usuari amb id = " + id + " no existeix", "UserSerice", "findOne");
+        }
+        return users.get(0);
+    }
+
+    public String uploadCsvUsers(MultipartFile csvFile) throws Exception{
+        int usersCreats = 0;
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(csvFile.getInputStream()));
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String[] arguments = line.split(",");
+                // Cambiar el formato de las entradas arguments
+                usersCreats += userRepository.save();
+            }
+        } catch{
+
+        }
+    }
 }
